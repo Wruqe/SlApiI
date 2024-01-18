45f65581cd13364dacc9e5d99b4278804309d471
TheOarsman <heinzulrichv@gmail.com>
2024-01-18 17:58:31 -0500

    starting seeds.sql lists

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE 
+
+INSERT INTO usernames (user_name, user_rating, first_name, last_name, email)
+VALUES
+
+INSERT INTO posts (post, post_rating)
+VALUES
+
+INSERT INTO comments (comment, reply, comment_rating)
+VALUES
