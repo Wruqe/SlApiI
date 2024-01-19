439570909d00df2c1ffda21d2bea0059a6f71b37
TheOarsman <heinzulrichv@gmail.com>
2024-01-18 21:09:35 -0500

    Seeds folder created

---
+++ seeds/seeds.js
@@ -0,0 +1,29 @@
+/// Still need to update this ///
+/// More to come ///
+
+const sequelize = require("../config/connection");
+const { User, Content, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const projectData = require("./projectData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  /// Seed users
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
