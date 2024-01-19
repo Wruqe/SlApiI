174d71c9e83e47b8266a89db46d4e33e82031e0d
Conor Allen <145404862+callen996@users.noreply.github.com>
2024-01-18 19:43:56 -0500

    Update express.js

--- controllers/api/express.js
+++ controllers/api/express.js
@@ -9,7 +9,7 @@ app.use('/user', router);
 
 // Access user routes: https://localhost:3000/users
 
-// Parse JSOn data : BUILT-INS
+// Parse JSON data : BUILT-INS
 app.use(express.json());
 
 // Authentication middleware (basic example): CUSTOM
@@ -18,4 +18,34 @@ app.use((req, res, next) => {
     return res.status(401) .send(' Unauthorized') ;
   }
   // vaildate authorization token and call next() if vaild next();
+});
+// Get all users
+app.get('/ users' , (req, res) => {
+  res.json({users: [ id: 1, name: 'John' } , ... ] });
+});
+// Get a specific user by ID
+app.get( '/users/:id' , (req, res) => {
+  const userID = req.params.id;
+  res.json({ id: userId, name: "User ${userId} ' });
+  });
+  // Create a new user
+        app.post( ' / users' , (req, res) => {
+          const newUser = req.body; // get data from request body
+                 // Save user to database and send response
+                 res.json({ mssage: "User created successfully!' })
+  });
+  const express = require(' express');
+  const app = express();
+  constant port = 3000;
+
+  // SIAPI! route
+   app.get('/' , (req, res) => res.send(' Express running!));
+// start server
+ app.listen(port, () => console.log('Server on port ${port} '));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if (err.status) {
+    res.status(err.status). send('Internal Server Error');
+  }
 });
