9aa9382656a20791d83cb3edc23e68c829db17c8
Conor Allen <145404862+callen996@users.noreply.github.com>
2024-01-18 19:20:07 -0500

    Update express.js

--- controllers/api/express.js
+++ controllers/api/express.js
@@ -2,3 +2,20 @@
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('User routes'));
+router.get('/:id' , (req, res) => res.send('User ID'));
+
+// Mount the User router on the '/users' path
+app.use('/user', router);
+
+// Access user routes: https://localhost:3000/users
+
+// Parse JSOn data : BUILT-INS
+app.use(express.json());
+
+// Authentication middleware (basic example): CUSTOM
+app.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401) .send(' Unauthorized') ;
+  }
+  // vaildate authorization token and call next() if vaild next();
+});
