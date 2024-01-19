7cb9c4f342045db832cc5cb48624cd77cdb78be4
Conor Allen <145404862+callen996@users.noreply.github.com>
2024-01-18 20:45:55 -0500

    Update express.js

--- controllers/api/express.js
+++ controllers/api/express.js
@@ -12,36 +12,6 @@ app.use('/user', router);
 // Parse JSON data : BUILT-INS
 app.use(express.json());
 
-// Authentication middleware (basic example): CUSTOM
-app.use((req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(401) .send(' Unauthorized') ;
-  }
-  // vaildate authorization token and call next() if vaild next();
-});
-// Get all users
-app.get('/ users' , (req, res) => {
-  res.json({users: [ id: 1, name: 'John' } , ... ] });
-});
-// Get a specific user by ID
-app.get( '/users/:id' , (req, res) => {
-  const userID = req.params.id;
-  res.json({ id: userId, name: "User ${userId} ' });
-  });
-  // Create a new user
-        app.post( ' / users' , (req, res) => {
-          const newUser = req.body; // get data from request body
-                 // Save user to database and send response
-                 res.json({ mssage: "User created successfully!' })
-  });
-  const express = require(' express');
-  const app = express();
-  constant port = 3000;
-
-  // SIAPI! route
-   app.get('/' , (req, res) => res.send(' Express running!));
-// start server
- app.listen(port, () => console.log('Server on port ${port} '));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
