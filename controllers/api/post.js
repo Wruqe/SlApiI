83001f1895b45e640b1bf4ed127c5e8231cb5c53
Whitney Simpson <whitmoon4819@gmail.com>
2024-01-22 19:05:19 -0500

    updated homepage.handlebars, profile.handlebars. and couple edits in the controllers file

---
+++ controllers/api/post.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const {
+    User, Post, Comment
+} = require('../../models')
+
+router.get('/', (req, res) => {
+    Post.findAll({include: [User, Comment]}).then(data => {res.json(data)})
+})
+
+router.post('/', (req, res)=>{
+    const postObject = {
+        content: req.body.content,
+        userId: req.session.userId
+    }
+    Post.create(postObject).then(postData => {
+        res.json(postData)
+    })
+})
+
+
+
+
+module.exports = router
