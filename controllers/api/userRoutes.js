832249aab28368ff4319a61ef56d11371c05cf1e
Whitney Simpson <whitmoon4819@gmail.com>
2024-01-25 11:55:15 -0500

    fixed logout after loggin in to re-direct to main page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -53,9 +53,10 @@ router.post('/logout', (req, res) => {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  } else {
-    res.status(404).end();
   }
+  // } else {
+  //   res.status(404).end();
+  // }
 });
 
 module.exports = router;
