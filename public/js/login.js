2da07862999a0113b08ad6faafe0145c52f0bd8f
Whitney Simpson <whitmoon4819@gmail.com>
2024-01-25 15:25:26 -0500

    fixed bug, needed module.exports, and chat box is relocated and working

--- public/js/login.js
+++ public/js/login.js
@@ -26,16 +26,16 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
+    const user_name = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     const first_name = document.querySelector('#first-name-signup').value.trim();
     const last_name = document.querySelector('#last-name-signup').value.trim();
   
-    if (username && first_name && last_name && email && password) {
+    if (user_name && first_name && last_name && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, first_name, last_name, email, password }),
+        body: JSON.stringify({ user_name, first_name, last_name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
       
