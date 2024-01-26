a59f5388a60c9c995187216fb8b300d071700110
Ernest Jones <ernest.jones30@yahoo.com>
2024-01-25 21:22:55 -0500

    Fixed rate btn with classes

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+const openChat = document.getElementById("openChat")
+ if (openChat) {
+     openChat.addEventListener('click', function () {
+         document.querySelector('#chat').removeAttribute('class');
+        })
+ }
+
